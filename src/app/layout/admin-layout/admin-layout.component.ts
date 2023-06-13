0cc966b1693b77bd0469cac6ddd021f64258d993
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-06-13 17:59:19 +0700

    feat:them sua xoa refs:N3-14

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { UserService } from 'src/app/seviceuser/user.service';
+import { Iproduct } from 'src/app/interface/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component( {
   selector: 'app-admin-layout',
@@ -8,9 +9,28 @@ import { UserService } from 'src/app/seviceuser/user.service';
 } )
 export class AdminLayoutComponent
 {
-  constructor ( private auth: UserService )
+  searchName: string = '';
+  product: Iproduct[] = [];
+  constructor ( private auth: ProductService )
   {
 
+  }
+  search ()
+  {
+    if ( this.searchName.trim() !== '' )
+    {
+      const keyword = this.searchName.toLowerCase();
+      this.auth.getProduct().subscribe( data =>
+      {
+        this.product = data.filter( item => item.name.toLowerCase().includes( keyword ) );
+      } );
+    } else
+    {
+      this.auth.getProduct().subscribe( data =>
+      {
+        this.product = data;
+      } );
+    }
   }
 
 }
